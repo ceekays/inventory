bf255b602b440fae3475e66fae51473e058693f5
ceekays <kachaleedmond@gmail.com>
2010-05-18 13:31:31 +0200

    added a method to redirect after printing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -74,4 +74,11 @@ class ApplicationController < ActionController::Base
        redirect_to(:action => "login" )
      end
   end
+
+  def print_and_redirect(print_url, redirect_url, message = "Printing, please wait...")
+    @print_url = print_url
+    @redirect_url = redirect_url
+    @message = message
+    render :template => 'item/print', :layout => nil
+  end
 end
