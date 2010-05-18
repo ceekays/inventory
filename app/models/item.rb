60e36b46289291c927d57d91af7b71bbc9a97eec
ceekays <kachaleedmond@gmail.com>
2010-05-18 13:41:18 +0200

    Added barcode handling

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ActiveRecord::Base
   has_many :statuses
-
+  before_create :bar_code
   def print_label(num = 1)
     # item_name
     # serial_number
@@ -12,7 +12,47 @@ class Item < ActiveRecord::Base
 	  label.draw_barcode(40, 180, 0, 1, 5, 15, 120, true, "#{self.barcode}")
 	  label.draw_text("Name: #{self.name.titleize}", 40, 30, 0, 2, 2, 2, false)
 	  label.draw_text("Serial Number: #{self.serial_number}", 40, 80, 0, 2, 2, 2, false)
-	  label.draw_text("Assigned to: #{self.assigned_to}", 40, 130, 0, 2, 2, 2, false)
+	  #label.draw_text("Collected y: #{self.collected_by rescue ''}", 40, 130, 0, 2, 2, 2, false)
 	  label.print(num)
+  end
+
+  def bar_code
+    if (self.barcode.blank? || self.barcode.nil?)
+      create_barcode
+    else
+       self.barcode
+    end
+  end
+
+  def formatted_bar_code
+     self.barcode[0..2]+"-"+self.barcode[3..5]+"-"+self.barcode[6..8]
+  end
+
+  def create_barcode
+    last_item     = Item.find(:first,:order=>"barcode desc")
+    last_barcode  = last_item.barcode rescue "0"
+    new_barcode   = (last_barcode[3..-2].to_i + 1).to_s.rjust(5,"0")
+    check_digit   = calculate_checkdigit(new_barcode)
+    new_barcode   = "BHT#{new_barcode}#{check_digit}"
+    self.barcode  = new_barcode
+  end
+
+  def calculate_checkdigit(number)
+    # This is Luhn's algorithm for checksums
+    # http://en.wikipedia.org/wiki/Luhn_algorithm
+    number = number.to_s
+    number = number.split(//).collect { |digit| digit.to_i }
+    parity = number.length % 2
+
+    sum = 0
+    number.each_with_index do |digit,index|
+      digit = digit * 2 if index%2==parity
+      digit = digit - 9 if digit > 9
+      sum = sum + digit
+    end
+
+    checkdigit = 0
+    checkdigit = checkdigit +1 while ((sum+(checkdigit))%10)!=0
+    return checkdigit
   end
 end
